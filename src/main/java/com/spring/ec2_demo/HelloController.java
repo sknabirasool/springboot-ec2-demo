f100626478e1abfcaf0366d17b6fc6b6b25a11ee
sknabirasool <sknabirasool104@gmail.com>
2026-06-28 22:43:54 +0530

    Initial Commit

---
+++ src/main/java/com/spring/ec2_demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.spring.ec2_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Spring Boot Application Running on AWS Ec2";
+    }
+}
---
+++ src/test/java/com/spring/ec2_demo/Ec2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ec2_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
