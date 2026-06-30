6aa151d433643ab5e74dc0c5201541fafcc886a4
sknabirasool <sknabirasool104@gmail.com>
2026-06-30 10:36:35 +0530

    ece config

--- src/main/java/com/spring/ec2_demo/HelloController.java
+++ src/main/java/com/spring/ec2_demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Spring Boot Application Running on AWS Ec2";
+        return "Spring Boot Application Running on AWS Ec2 Test";
     }
 }
